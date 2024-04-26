845bdf25805393329428ff94866466fc64cf2f74
saiful797 <saifulislamsaif797@gmail.com>
2024-04-26 09:43:12 +0600

    Get data from add tourist spot form successfully

--- src/pages/AddTourismSpotPage.jsx
+++ src/pages/AddTourismSpotPage.jsx
@@ -1,24 +1,32 @@
+import { useForm} from "react-hook-form"
 
 const AddTourismSpotPage = () => {
+
+    const{register, handleSubmit} = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data)
+        
+    }
    
     return (
         <div className="p-5 md:p-16">
             <h1 className="text-2xl md:text-5xl font-extrabold mb-5 text-center">Add a Tourist Spot</h1>
             
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 {/* form user name and email row */}
                 <div className="md:flex gap-5 md:mb-8">
                     <div className="form-control md:w-1/2 mb-5 md:mb-0">
                         <label className="label">
                             <span className="label-text text-lg font-medium">User Name</span>
                         </label>
-                        <input type="text" name="name" placeholder="Enter Your Name..." className="input input-bordered" required />
+                        <input type="text" name="name" placeholder="Enter Your Name..." className="input input-bordered" {...register("name")} required />
                     </div>
                     <div className="form-control md:w-1/2 mb-5 md:mb-0">
                         <label className="label">
                             <span className="label-text text-lg font-medium">User Email</span>
                         </label>
-                        <input type="text" name="email" placeholder="Enter Your Email..." className="input input-bordered" required />
+                        <input type="email" name="email" placeholder="Enter Your Email..." className="input input-bordered" {...register("email")} required />
                     </div>
                 </div>
 
@@ -28,13 +36,13 @@ const AddTourismSpotPage = () => {
                         <label className="label">
                             <span className="label-text text-lg font-medium">Spot Name</span>
                         </label>
-                        <input type="text" name="spot" placeholder="Enter Tourists Spot Name..." className="input input-bordered" required />
+                        <input type="text" name="spot" placeholder="Enter Tourists Spot Name..." className="input input-bordered" {...register("spot")} required />
                     </div>
                     <div className="form-control md:w-1/2 mb-5 md:mb-0">
                         <label className="label">
                             <span className="label-text text-lg font-medium">Spot Location</span>
                         </label>
-                        <input type="text" name="location" placeholder="Enter Spot Location..." className="input input-bordered" required />
+                        <input type="location" name="location" placeholder="Enter Spot Location..." className="input input-bordered" {...register("location")} required />
                     </div>
                 </div>
 
@@ -44,13 +52,13 @@ const AddTourismSpotPage = () => {
                         <label className="label">
                             <span className="label-text text-lg font-medium">Seasonality</span>
                         </label>
-                        <input type="text" name="seasonality" placeholder="Enter Season Name(ex. summer)..." className="input input-bordered" required />
+                        <input type="text" name="seasonality" placeholder="Enter Season Name(ex. summer)..." className="input input-bordered" {...register("seasonality")} required />
                     </div>
                     <div className="form-control md:w-1/2 mb-5 md:mb-0">
                         <label className="label">
                             <span className="label-text text-lg font-medium">Average Cost</span>
                         </label>
-                        <input type="text" name="cost" placeholder="Enter Average Cost..." className="input input-bordered" required />
+                        <input type="number" name="cost" placeholder="Enter Average Cost..." className="input input-bordered" {...register("cost")}  required />
                     </div>
                 </div>
 
@@ -60,14 +68,14 @@ const AddTourismSpotPage = () => {
                         <label className="label">
                             <span className="label-text text-lg font-medium">Travel Time</span>
                         </label>
-                        <input type="text" name="time" placeholder="Enter Travel Time(ex. 7 days)..." className="input input-bordered" required />
+                        <input type="text" name="time" placeholder="Enter Travel Time(ex. 7 days)..." className="input input-bordered" {...register("time")} required />
                     </div>
 
                     <div className="form-control md:w-1/2 mb-5 md:mb-0">
                         <label className="label">
                             <span className="label-text text-lg font-medium">Total Visitors Per Year</span>
                         </label>
-                        <input type="text" name="visitors" placeholder="Enter total Visitors Per Year.." className="input input-bordered" required />
+                        <input type="number" name="visitors" placeholder="Enter total Visitors Per Year.." className="input input-bordered" {...register("visitors")}required />
                     </div>
                 </div>
 
@@ -76,7 +84,7 @@ const AddTourismSpotPage = () => {
                     <label className="label">
                         <span className="label-text text-lg font-medium">Short Description</span>
                     </label>
-                    <textarea placeholder="Enter Spot Short Description..." className="textarea textarea-bordered textarea-lg w-full" ></textarea>
+                    <textarea type="text" name="description" placeholder="Enter Spot Short Description..." className="textarea textarea-bordered textarea-lg w-full" {...register("description")} required ></textarea>
                 </div>
 
                 {/* Photo row */}
@@ -84,7 +92,7 @@ const AddTourismSpotPage = () => {
                     <label className="label">
                         <span className="label-text text-lg font-medium">Spot Photo URL</span>
                     </label>
-                    <input type="url" name="photo" placeholder="Enter Tourist Spot Photo URL..." className="input input-bordered" required />
+                    <input type="url" name="photo" placeholder="Enter Tourist Spot Photo URL..." className="input input-bordered" {...register("photo")} required />
                 </div>
 
                 <input type="submit" value="ADD SPOT" className="btn w-full bg-black text-white" />
