58a54b4db066ed0b4945beacacb670f29d9fc12e
saiful797 <saifulislamsaif797@gmail.com>
2024-04-26 12:13:44 +0600

    Get data from mongodb server

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,10 @@
+import { useLoaderData } from "react-router-dom";
 
 const HomePage = () => {
+    const tourismSpots = useLoaderData();
+
+    console.log(tourismSpots);
+    
     return (
         <div>
             <h1 className="text-6xl font-extrabold text-center">This is Home Page.</h1>
