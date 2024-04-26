76d0701d7683169be7c2a7a96fc5544f585c1d19
saiful797 <saifulislamsaif797@gmail.com>
2024-04-27 01:05:07 +0600

    Added country section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
+import CountriesCard from "../components/CountriesCard/CountriesCard";
 import SliderSwiper from "../components/sliderSwiper/SliderSwiper";
 import TouristsSpotCard from "../components/spots/TouristsSpotCard";
 import { useEffect, useState } from "react";
 
 const HomePage = () => {
     const [spots, setSpot] = useState([]);
+    const [countries, setCountries] = useState([]);
 
     useEffect(() => {
         fetch('touristsSport.json')
@@ -11,6 +13,14 @@ const HomePage = () => {
          .then(data =>{
             setSpot(data);
          })
+    })
+
+    useEffect(() => {
+        fetch("countries.json")
+          .then(res => res.json())
+          .then(data => {
+            setCountries(data);
+          }) 
     })
 
     return (
@@ -25,6 +35,17 @@ const HomePage = () => {
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 place-items-center gap-5">
                     {
                         spots.map(spot => <TouristsSpotCard key={spot.id} spot={spot} />)
+                    }
+                </div>
+            </div>
+
+            <div className="mt-10">
+                <div className="bg-black p-3 mb-10">
+                    <h1 className="text-4xl font-extrabold text-center text-white">Countries </h1>
+                </div>
+                <div className="grid lg:grid-cols-2 place-items-center gap-6">
+                    {
+                        countries.map(country => <CountriesCard key={country.id} country={country} />)
                     }
                 </div>
             </div>
