ccceb7d4ed419abccf44ce913a0da08b8809e83b
saiful797 <saifulislamsaif797@gmail.com>
2024-04-28 16:17:29 +0600

    fixed my list page bug

--- src/pages/MyListPage.jsx
+++ src/pages/MyListPage.jsx
@@ -14,10 +14,15 @@ const MyListPage = () => {
             setMyList(data);
         })
     },[user])
+
+    const newMyList = myList.filter(spot => user.email === spot.userEmail);
+
+    // console.log(newMyList);
+
     return (
         <div className="mx-auto grid md:grid-cols-2 lg:grid-cols-3">
             {
-                myList.map(touristSpot => <AllTouristSpotCard key={touristSpot._id} touristSpot={touristSpot} />)
+                newMyList.map(touristSpot => <AllTouristSpotCard key={touristSpot._id} touristSpot={touristSpot} />)
             }
         </div>
     );
