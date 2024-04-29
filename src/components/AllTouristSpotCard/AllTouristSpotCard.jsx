e45139ed6344a865aff8f1aed791f6d3ecb9ae28
saiful797 <saifulislamsaif797@gmail.com>
2024-04-29 17:19:51 +0600

    My List page info update and delate perfectly

--- src/components/AllTouristSpotCard/AllTouristSpotCard.jsx
+++ src/components/AllTouristSpotCard/AllTouristSpotCard.jsx
@@ -1,47 +1,9 @@
 import PropTypes from 'prop-types'
-import { useContext } from 'react';
 import { Link} from 'react-router-dom';
-import { AuthContext } from '../../Context/firebaseProvider/FirebaseProvider';
-import UpdateTouristSpotPage from "../UpdateTouristSpotModal/UpdateTouristSpotPage";
-import Swal from 'sweetalert2';
+
 
 const AllTouristSpotCard = ({touristSpot}) => {
-    const {user} = useContext(AuthContext);
-    const {_id, photo, cost, seasonality, spot, time, userEmail} = touristSpot;
-
-    const handleTouristSpotDelete = (id) =>{
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-          }).then((result) => {
-            if (result.isConfirmed) {
-              fetch(`http://localhost:5000/deleteSpot/${id}`,{
-                method: 'DELETE',
-              })
-                .then(res => res.json())
-                .then(data =>{
-                    // alert(data.deletedCount);
-                    if(data.deletedCount > 0){
-                        Swal.fire({
-                            title: "Deleted!",
-                            text: "Your tourist spot has been deleted.",
-                            icon: "success"
-                        });
-
-                        location.origin()
-                    }
-                    // console.log(data);
-                    // location.reload()
-                })
-            }
-        });
-
-    }
+    const {_id, photo, cost, seasonality, spot, time} = touristSpot;
 
     return (
         <div className={`w-[320px] h-[570px] glass mt-10 shadow-2xl mx-auto`}>
@@ -59,37 +21,6 @@ const AllTouristSpotCard = ({touristSpot}) => {
                         <button className="btn btn-sm btn-outline  w-full">View Details</button> 
                     </Link>
                 </div>
-                    {
-                        (user?.email=== userEmail)?<div className="flex justify-between items-center gap-1">
-                            {/* <button className="btn btn-sm btn-outline">
-                                Update This Spot 
-                            </button> */}
-                            {/* You can open the modal using document.getElementById('ID').showModal() method */}
-                            <button className="btn btn-sm btn-outline btn-success w-1/2" onClick={()=>document.getElementById('my_modal_4').showModal()}>Update</button>
-                            <dialog id="my_modal_4" className="modal">
-                                <div className="modal-box w-full max-w-6xl">
-                                    <div>
-                                        <UpdateTouristSpotPage _id={_id} />
-                                    </div>
-                                    <div className="modal-action flex justify-center items-center">
-                                        <form method="dialog w-full">
-                                            {/* if there is a button, it will close the modal */}
-                                            <button className="btn btn-sm btn-outline text-xl w-full bg-black text-white">X</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </dialog>
-
-                            <div className="w-1/2">
-                                <button onClick={() => handleTouristSpotDelete(_id)} className="btn btn-sm btn-outline btn-error w-full">
-                                    Delete
-                                </button>
-                            </div>
-                        </div>
-                        
-                        :
-                        ''
-                    }
             </div>
         </div>
     );
--- src/pages/MyListPage.jsx
+++ src/pages/MyListPage.jsx
@@ -1,6 +1,9 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Context/firebaseProvider/FirebaseProvider";
-import MyListReactTable from "../components/MyListReactTable/MyListReactTable";
+import { MdDeleteForever } from "react-icons/md";
+import { AiOutlineEdit } from "react-icons/ai";
+import Swal from "sweetalert2";
+import UpdateTouristSpotPage from "../components/UpdateTouristSpotModal/UpdateTouristSpotPage";
 
 const MyListPage = () => {
     const {user} = useContext(AuthContext);
@@ -17,11 +20,85 @@ const MyListPage = () => {
 
     const newMyList = myList.filter(spot => user.email === spot.userEmail);
     // console.log(newMyList)
-    return (
-        <div className="mx-auto grid md:grid-cols-2 lg:grid-cols-3">
-            {
-                newMyList.map(touristSpot => <MyListReactTable key={touristSpot._id} touristSpot={touristSpot} />)
+    const handleTouristSpotDelete = (id) =>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              fetch(`http://localhost:5000/deleteSpot/${id}`,{
+                method: 'DELETE',
+              })
+                .then(res => res.json())
+                .then(data =>{
+                    // alert(data.deletedCount);
+                    if(data.deletedCount > 0){
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your tourist spot has been deleted.",
+                            icon: "success"
+                        });
+                        location.reload()
+                    }
+                    // console.log(data);
+                    // location.reload()
+                })
             }
+        });
+    }
+
+    return (
+        <div className="mx-auto container mt-5">
+            <table className="table">
+                <thead className="text-black">
+                    <th>ID</th>
+                    <th>Spot Name</th>
+                    <th>Country</th>
+                    <th>Average Cost</th>
+                    <th>Action</th>
+                </thead>
+
+                <tbody>
+                    {
+                        newMyList.map((list, id) => (
+                            
+                            <tr key={id} className="">
+                                <td>{id+1}</td>
+                                <td>{list.spot}</td>
+                                <td>{list.country}</td>
+                                <td>${list.cost}</td>
+                                
+                                <td className="flex gap-1">
+                                    <div className="flex justify-between items-center gap-1">
+                                        {/* You can open the modal using document.getElementById('ID').showModal() method */}
+                                        <button className="btn btn-sm btn-outline" onClick={()=>document.getElementById('my_modal_4').showModal()}><AiOutlineEdit /></button>
+                                        <dialog id="my_modal_4" className="modal">
+                                            <div className="modal-box w-full max-w-6xl">
+                                                <div>
+                                                    <UpdateTouristSpotPage _id={list._id} />
+                                                </div>
+                                                <div className="modal-action flex justify-center items-center">
+                                                    <form method="dialog w-full">
+                                                        {/* if there is a button, it will close the modal */}
+                                                        <button className="btn btn-sm btn-outline text-xl w-full bg-black text-white">X</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </dialog>
+                                    </div>
+
+                                    <button onClick={() => handleTouristSpotDelete(list._id)} className="btn btn-outline btn-sm text-red-500 text-lg"><MdDeleteForever /></button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
