d204a004d1d3ade25acb0c06951aaa9e00710494
saiful797 <saifulislamsaif797@gmail.com>
2024-04-30 22:18:27 +0600

    Updated private route

--- src/protectedRoute/ProtectedRoute.jsx
+++ src/protectedRoute/ProtectedRoute.jsx
@@ -3,14 +3,14 @@ import { AuthContext } from "../Context/firebaseProvider/FirebaseProvider";
 import { Navigate, useLocation } from "react-router-dom";
 import PropTypes from 'prop-types'
 
-
 const ProtectedRoute = ({ children }) => {
-
     const {user, loading} = useContext(AuthContext) || {};
     const location = useLocation();
 
     if(loading) return <span className="loading loading-spinner loading-lg"></span>
+
     if(user) return children ;
+
     return <Navigate state={location?.pathname || '/'} to={`/login`} replace/>
 };
 
